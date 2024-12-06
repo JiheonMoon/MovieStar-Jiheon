8194ce24ee31430ec1db6fe7612c8ea3aa820620
Jiheon <wlgjs566@gmail.com>
2024-12-06 16:39:36 +0900

    actors 추가

--- backend/moviestar/src/main/java/com/korea/moviestar/entity/MovieEntity.java
+++ backend/moviestar/src/main/java/com/korea/moviestar/entity/MovieEntity.java
@@ -1,13 +1,16 @@
 package com.korea.moviestar.entity;
 
+import java.util.List;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,18 +25,21 @@ import lombok.NoArgsConstructor;
 @Table(name = "movie")
 public class MovieEntity {
 	@Id
-	int movieId;
-	String movieName;
+	private int movieId;
+	private String movieName;
 	@ManyToMany
 	@JoinTable(
 			name = "movie_theme_table",
 			joinColumns = @JoinColumn(name="movie_id"),
 			inverseJoinColumns = @JoinColumn(name = "theme_id")
 	)
-	Set<ThemeEntity> movieTheme;
-	String movieOpDate;
-	double movieScore;
-	String moviePoster;
-	@Column(length = 65535)
-	String movieOverview;
+	private Set<ThemeEntity> movieTheme;
+	private String movieOpDate;
+	private double movieScore;
+	private String moviePoster;
+	@Column(length = 65535, columnDefinition = "TEXT")
+	private String movieOverview;
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "movie_id")
+	private List<ActorEntity> movieActors;
 }

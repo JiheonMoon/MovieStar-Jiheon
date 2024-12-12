ffc28a4725f01481ae9db753d76551dd21368327
Jiheon <wlgjs566@gmail.com>
2024-12-12 10:45:59 +0900

    email 비밀번호 변경

--- backend/moviestar/src/main/java/com/korea/moviestar/service/MailService.java
+++ backend/moviestar/src/main/java/com/korea/moviestar/service/MailService.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.korea.moviestar.entity.MailVerificationEntity;
+import com.korea.moviestar.entity.UserEntity;
 import com.korea.moviestar.repo.MailVerificationRepository;
+import com.korea.moviestar.repo.UserRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MailService {
 	private final JavaMailSender emailSender;
 	private final MailVerificationRepository repository;
+	private final UserRepository users;
 
     public void sendEmail(String toEmail,
                           String title,
@@ -47,6 +51,10 @@ public class MailService {
     }
     
     public void sendVerificationCode(String email) {
+    	if(!users.existsByUserEmail(email)) {
+    		throw new RuntimeException("존재하지 않는 이메일입니다");
+    	}
+    	
         String code = Integer.toString((int)(Math.random() * 900000) + 100000);
         
         MailVerificationEntity entity = MailVerificationEntity.builder()
@@ -56,11 +64,11 @@ public class MailService {
         		.build();
         repository.save(entity);
         
-        String message = "이메일 인증을 위해 아래 코드를 입력하세요:\n\n" + code;
+        String message = "이메일 인증을 위해 아래 코드를 입력하세요(보내주신 코드는 임시 비밀번호로 사용됩니다):\n\n" + code;
         sendEmail(email, "이메일 인증 코드", message);
     }
     
-    public boolean verifyCode(String email, String inputCode) {
+    public boolean verifyCode(String email, String inputCode, PasswordEncoder encoder) {
         MailVerificationEntity entity = repository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("인증 요청이 없습니다."));
 
@@ -71,7 +79,11 @@ public class MailService {
         if (!entity.getCode().equals(inputCode)) {
             throw new RuntimeException("인증 코드가 일치하지 않습니다.");
         }
-
+        
+        UserEntity user = users.findByUserEmail(email).get();
+        user.setUserPwd(encoder.encode(inputCode));
+        users.save(user);
+        
         repository.delete(entity);
         return true;
     }

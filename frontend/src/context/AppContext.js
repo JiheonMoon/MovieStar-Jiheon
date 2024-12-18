bab9f7de7022e6c67fe274cc8853ae7c7dd44fb4
zlzkzlgh <zlzkzlgh@gmail.com>
2024-12-18 15:19:36 +0900

    마이페이지에서 좋아요 상세페이지 데이터 못불러왔던거 수정
    마이페이지에서 좋아요 상세페이지 데이터 못불러왔던거 수정

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -1,5 +1,6 @@
 // MainScreen에서 로그인 여부에 따라 버튼을 로그인 <-> 로그아웃
 // 이를 위해 Context 활용
+// AppContext.js
 import React, { createContext, useState } from 'react';
 
 export const AppContext = createContext();
@@ -14,20 +15,40 @@ const AppProvider = ({ children }) => {
         return user?.userLikeList?.some((movie) => movie.id === movieId);
     };
 
-    // 좋아요 추가 함수
+    // 좋아요 추가 함수 - 필요한 영화 정보만 저장
     const addLikeMovie = (movie) => {
-        const updatedLikes = [...(user.userLikeList || []), movie];
-        setUser({ ...user, userLikeList: updatedLikes });
+        const movieData = {
+            id: movie.id,
+            title: movie.title,
+            poster_path: movie.poster_path,
+            overview: movie.overview,
+            release_date: movie.release_date,
+            vote_average: movie.vote_average
+        };
+        const updatedLikes = [...(user.userLikeList || []), movieData];
+        setUser(prevUser => ({
+            ...prevUser,
+            userLikeList: updatedLikes
+        }));
     };
 
     // 좋아요 제거 함수
     const removeLikeMovie = (movieId) => {
         const updatedLikes = user.userLikeList.filter((movie) => movie.id !== movieId);
-        setUser({ ...user, userLikeList: updatedLikes });
+        setUser(prevUser => ({
+            ...prevUser,
+            userLikeList: updatedLikes
+        }));
     };
 
     return (
-        <AppContext.Provider value={{ user, setUser, isMovieLiked, addLikeMovie, removeLikeMovie }}>
+        <AppContext.Provider value={{ 
+            user, 
+            setUser, 
+            isMovieLiked, 
+            addLikeMovie, 
+            removeLikeMovie 
+        }}>
             {children}
         </AppContext.Provider>
     );
